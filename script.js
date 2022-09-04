7c86e3a2d1723025045d0bb3ca37f67828bf92fe
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-04 20:12:00 +0300

    Optimization#2

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var foot = document.querySelector("footer");
 var cil = document.getElementById("cil");
 var feh = document.getElementById("feh");
-let currDeg = document.querySelectorAll(".tt");
-console.log(currDeg);
+let currDeg = document.querySelectorAll(".tt span");
 function hideFoot() {
   foot.remove();
 }
@@ -10,18 +9,19 @@ function alerting() {
   alert("Loading Weather report");
 }
 function changeToC(input) {
-  return (input - 32) * 0.5556;
+  return Math.floor((input - 32) * 0.5556);
 }
 // console.log(changeToC(60));
 function changeToF(input) {
-  return input * (9 / 5) + 32;
+  return Math.floor(input * (9 / 5) + 32);
 }
-// console.log(changeToC(100));
+// console.log(changeToF(100));
 function chDeg(value) {
   if (value == "cil")
-    for (var i = 1; i < currDeg.length; i++)
-      currDeg[i].innerText = changeToF(parseInt(currDeg[i - 1]).innerText);
+    for (var i = 0; i < currDeg.length; i++) {
+      currDeg[i].innerText = changeToC(currDeg[i].innerText);
+    }
   else if (value == "feh")
-    for (var j = 1; j < currDeg.length; j++)
-      currDeg[j].innerText = changeToC(parseInt(currDeg[j - 1]).innerText);
+    for (var j = 0; j < currDeg.length; j++)
+      currDeg[j].innerText = changeToF(currDeg[j].innerText);
 }
